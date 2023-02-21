68bc2f07b78a5c1a27f52ca3c1f9dd6a7fa87240
Manav <90278748+Manav011@users.noreply.github.com>
2023-02-21 22:49:54 +0530

    Create A.js

---
+++ Animate/A.js
@@ -0,0 +1,19 @@
+const al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover = event => {
+  let iterations = 0;
+  
+  const interval = setInterval(() => {
+  event.target.innerText = event.target.innerText.split("").map((letter , index) => {
+            if(index < iterations){
+              return event.target.dataset.value[index];
+            }               
+    
+              return al[Math.floor(Math.random() * 26)]
+  })
+                 .join("");
+    
+    if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1 / 7;
+  }, 30);
+}
